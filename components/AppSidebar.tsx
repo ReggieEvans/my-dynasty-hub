b806d05e36e366c06237d8a8165f9711fd2acdc3
Reggie Evans <REvans911@gmail.com>
2025-06-17 21:50:33 -0500

    switching computers

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -1,18 +1,122 @@
+'use client';
+
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
-} from "@/components/ui/sidebar";
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  useSidebar,
+} from '@/components/ui/sidebar';
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  ChevronRight,
+  ChevronsUpDown,
+  Command,
+  GalleryVerticalEnd,
+  Plus,
+  Settings2,
+  SquareTerminal,
+} from 'lucide-react';
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible';
+import {
+  DropdownMenu,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@radix-ui/react-dropdown-menu';
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuShortcut,
+} from './ui/dropdown-menu';
+import { useState } from 'react';
+
+// This is sample data.
+const data = {
+  user: {
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  teams: [
+    {
+      name: 'Acme Inc',
+      logo: GalleryVerticalEnd,
+      plan: 'Enterprise',
+    },
+    {
+      name: 'Acme Corp.',
+      logo: AudioWaveform,
+      plan: 'Startup',
+    },
+    {
+      name: 'Evil Corp.',
+      logo: Command,
+      plan: 'Free',
+    },
+  ],
+  navMain: [
+    {
+      title: 'Playground',
+      url: '#',
+      icon: SquareTerminal,
+      isActive: true,
+    },
+    {
+      title: 'Models',
+      url: '#',
+      icon: Bot,
+    },
+    {
+      title: 'Documentation',
+      url: '#',
+      icon: BookOpen,
+    },
+    {
+      title: 'Settings',
+      url: '#',
+      icon: Settings2,
+    },
+  ],
+};
 
 export function AppSidebar() {
+  const { isMobile } = useSidebar();
+  const [activeTeam, setActiveTeam] = useState(data.teams[0]);
+
   return (
-    <Sidebar>
-      <SidebarHeader>Team Name Here</SidebarHeader>
+    <Sidebar collapsible='icon' className='mt-[68px]'>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarMenu>
+          {data.navMain.map((item) => (
+            <Collapsible
+              key={item.title}
+              asChild
+              defaultOpen={item.isActive}
+              className='group/collapsible'
+            >
+              <SidebarMenuItem>
+                <SidebarMenuButton tooltip={item.title}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </Collapsible>
+          ))}
+        </SidebarMenu>
       </SidebarContent>
       <SidebarFooter>User Info Here</SidebarFooter>
     </Sidebar>
