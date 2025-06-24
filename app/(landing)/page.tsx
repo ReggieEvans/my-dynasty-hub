9f1963af2d7acb69213b1e2b3302eac2b9c80b38
Reggie Evans <REvans911@gmail.com>
2025-06-24 09:37:27 -0500

    Updates sign up page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,49 +1,3 @@
-// import Image from "next/image";
-
-// export default function LandingPage() {
-//   return (
-//     <main className="min-h-screen text-foreground flex flex-col items-center justify-center p-6">
-//       <div className="flex justify-center py-4">
-//         <Image
-//           src="/logo-389-47.png"
-//           alt="Next.js Logo"
-//           width={400}
-//           height={47}
-//           priority
-//         />
-//       </div>
-//       <p className="text-xl md:text-xl mb-6 text-center max-w-md">
-//         The ultimate tool to manage and track your EA Sports College Football
-//         Dynasty.
-//       </p>
-
-//       <form
-//         action="https://formspree.io/f/YOUR-FORM-ID"
-//         method="POST"
-//         className="w-full max-w-md flex flex-col items-center gap-3"
-//       >
-//         <input
-//           type="email"
-//           name="email"
-//           placeholder="Enter your email"
-//           className="w-full p-3 rounded border shadow-sm bg-input opacity-70"
-//           required
-//         />
-//         <button
-//           type="submit"
-//           className="bg-gradient-to-br from-primary to-primary-gradient w-full text-foreground font-semibold px-6 py-3 rounded hover:brightness-90 transition"
-//         >
-//           Notify Me
-//         </button>
-//       </form>
-
-//       <p className="text-muted-foreground mt-6 text-sm">
-//         Beta Launching Summer 2025. Follow us on Twitter @MyDynastyHub
-//       </p>
-//     </main>
-//   );
-// }
-
 "use client";
 
 import { Instagram, Twitter } from "lucide-react";
@@ -101,9 +55,13 @@ export default function LandingPage() {
           />
         </div>
 
-        <p className="text-foreground text-center text-xl max-w-md">
-          The ultimate tool to manage and track your EA Sports College Football
-          Dynasty.
+        <p className="text-foreground text-lg max-w-xl text-center mb-4">
+          MyDynastyHub is the ultimate companion tool for managing your EA
+          Sports College Football Dynasty with powerful tools and immersive
+          features — no more spreadsheets.
+        </p>
+        <p className="text-primary text-lg max-w-xl text-center">
+          Join the waitlist to be the first to know when it&apos;s ready.
         </p>
       </div>
 
@@ -127,7 +85,7 @@ export default function LandingPage() {
             disabled={submitting || !email}
             className="btn-primary text-lg"
           >
-            {submitting ? "Submitting..." : "Notify Me"}
+            {submitting ? "Submitting..." : "Join the waitlist"}
           </Button>
         ) : (
           <p className="text-primary font-medium text-center">
@@ -138,24 +96,33 @@ export default function LandingPage() {
       </form>
 
       <div className="text-muted-foreground mt-6 text-sm flex flex-col items-center gap-4 text-center">
-        <p>Building in public. Follow along on Instagram or Twitter.</p>
-        <div className="flex items-center gap-8">
-          <div>
+        <p>Building in public. Follow along on Instagram or X.</p>
+        <div className="flex flex-col items-center gap-8">
+          <div className="btn-primary rounded-full py-2 px-6">
+            <a
+              href="https://x.com/dynastyhubapp"
+              target="_blank"
+              className="text-foreground flex items-center gap-2"
+            >
+              <Twitter /> @dynastyhubapp
+            </a>
+          </div>
+          <div className="btn-primary rounded-full py-2 px-6">
             <a
               href="https://x.com/reggieevansdev"
               target="_blank"
-              className="text-primary"
+              className="text-foreground flex items-center gap-2"
             >
-              <Twitter />
+              <Twitter /> @reggieevansdev
             </a>
           </div>
-          <div>
+          <div className="btn-primary rounded-full py-2 px-6">
             <a
               href="https://www.instagram.com/reggieevans.dev"
               target="_blank"
-              className="text-primary"
+              className="text-foreground flex items-center gap-2"
             >
-              <Instagram />
+              <Instagram /> @reggieevans.dev
             </a>
           </div>
         </div>
